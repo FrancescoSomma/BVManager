ee57fe438fc8e822b4ace20492acb3d807d16b2a
fsomma <francesco.somma@dxc.com>
2023-10-08 16:34:50 +0200

    refactor

--- bvm-parent/bvm-microservizi/bvm-autenticazione/src/main/java/com/example/bvmautenticazione/BvmAutenticazioneApplication.java
+++ bvm-parent/bvm-microservizi/bvm-autenticazione/src/main/java/com/example/bvmautenticazione/BvmAutenticazioneApplication.java
@@ -1,13 +1,13 @@
 package com.example.bvmautenticazione;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class BvmAutenticazioneApplication {
+public class ServletInitializer extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BvmAutenticazioneApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BvmAutenticazioneApplication.class);
     }
 
 }
